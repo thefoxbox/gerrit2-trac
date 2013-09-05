9b1cd1ce51031356a79e501472f5fa2b91072787
Eric Fox <eric@thefoxbox.com>
2013-09-05 12:40:42 -0700

    Initial Gerrit2Trac v1.0.0 release.

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-gerrit2-trac
-============
+# Gerrit2Trac
+
+Gerrit2Trac is a utility for updating Trac tickets via Gerrit Code Review hooks.
+
+## Requirements
+
+* Python >= 2.6.x
+* [Gerrit](http://code.google.com/p/gerrit/) >= 2.7.x
+* [Trac](http://trac.edgewall.org) >= 0.12.x
+* [Trac XML-RPC Plugin](http://trac-hacks.org/wiki/XmlRpcPlugin)
+
+## Assumptions
+
+* A working knowledge of \*nix, Gerrit, Trac and Git.
+* Gerrit's $site\_path is _/opt/gerrit2_.
+* Login name (or UID) the Gerrit JVM executes as is _gerrit2_.
+* _gerrit2_ is a valid Trac user with TICKET\_MODIFY privileges.
+
+## Configuration
+
+Clone the Gerrit2Trac repository.
+
+    git clone https://github.com/thefoxbox/gerrit2-trac.git
+
+Copy the included sample configuration to your $site\_path/etc directory and protect the file contents.
+
+    sudo cp ./gerrit2-trac/examples/gerrit2trac.config.sample /opt/gerrit2/etc/gerrit2trac.config
+    sudo chown gerrit2:gerrit2 /opt/gerrit2/etc/gerrit2trac.config
+    sudo chmod 0600 /opt/gerrit2/etc/gerrit2trac.config
+
+Modify the configuration file _/opt/gerrit2/etc/gerrit2trac.config_, specifying the correct username, password and URLs for your site.
+
+Copy the `gerrit2trac.py` script to your $site\_path/bin directory.
+
+    sudo cp ./gerrit2-trac/gerrit2trac.py /opt/gerrit2/bin
+
+Create the desired Gerrit Hooks (_patchset-created_, _change-merged_, etc.) in your $site\_path/hooks directory.
+
+    if [ ! -d /opt/gerrit2/hooks ]; then
+        sudo mkdir /opt/gerrit2/hooks
+    fi
+
+    for hook in patchset-created reviewer-added comment-added change-merged; do
+        if [ ! -e /opt/gerrit2/hooks/${hook} ]; then
+            sudo cp ./gerrit2-trac/examples/patchset-created.sample /opt/gerrit2/hooks/${hook}
+            sudo chmod 0755 /opt/gerrit2/hooks/${hook}
+        fi
+    done
+
+## Usage
+
+Once the Gerrit hooks have been created, any corresponding Gerrit action will update the Trac ticket referenced in the Git commit message.
+
+## Workflow customization
+
+Trac ticket update attributes _comment_, _action_ and _cc_  can be customized in the _[workflow]_ section of $site\_path/etc/gerrit2trac.config.
+See the following examples for a sample custom workflow.
+
+Variable substitution in comments is supported where _$variable_ is an available parameter.
+See `gerrit2trac.py -h` for a list of available parameters.
+
+See the Gerrit Hook [documentation](https://gerrit-review.googlesource.com/Documentation/config-hooks.html) for a list of
+available hooks and the associated parameters.
+
+Note: When using variable substitution, hyphenated parameters must be referenced without the hyphen.
+
+## Examples
+
+### Example Git commit message:
+
+    This is an example Git commit message.
+
+    Including a line with 'Ticket: #' will update an existing Trac ticket
+    with pre-configured defaults or customized ticket actions.
+
+    Customized ticket actions can be confgured in the [workflow] section
+    of gerrit2trac.config.
+
+    Ticket: 101
+    Change-Id: I3ab5940714c59d81ed14fa22df1d5e506cd20cdd
+
+### Example custom workflow:
+
+    [workflow]
+    ;
+    ; An example patchset-created triggerd workflow
+    ;
+    ; * Adds a custom Gerrit comment using variable substitution and wiki formatting.
+    ; * Adds the patch-set uploader to the ticket Cc: field.
+    ;
+    ; Note: remove the hyphen from hyphenated parameters.
+    ;
+    ; See ./gerrit2-trac/examples/gerrit2trac.config.sample for more examples.
+    ;
+    patchset_created.comment = A new patch-set is awaiting review.
+        * Change-Id: $change
+        * Change-Url: $changeurl
+        * Patch-Set: $patchset
+        * Uploader: $uploader
+    patchset_created.action = leave
+    patchset_created.cc = $uploader
+
---
+++ gerrit2trac.py
@@ -0,0 +1,298 @@
+#!/usr/bin/python
+#
+# gerrit2trac.py - Update Trac tickets via Gerrit Code Review hooks
+#
+# Copyright (C) 2013 Eric Fox <eric@thefoxbox.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Gerrit2Trac
+
+A Python utility for updating Trac tickets via Gerrit Code Review hooks.
+"""
+
+__version__ = '1.0.0'
+
+import ConfigParser
+import json
+import re
+import sys
+from email.utils import parseaddr
+from optparse import OptionParser, OptionGroup
+from string import Template
+from time import time
+from urllib2 import urlopen, HTTPError, URLError
+from urlparse import urlparse
+from xmlrpclib import ServerProxy, ProtocolError
+
+
+def process_options(arglist=None):
+    """
+    Process options passed via command line args.
+
+    These are "most" of the options listed in the Gerrit hooks documentation.
+    """
+    usage = 'Usage: %prog [option] [hook] <parameter>'
+    parser = OptionParser(version=__version__, usage=usage)
+    parser.add_option('-v',
+                      action='store_true', help='verbose output', default=False)
+    parser.add_option('-c',
+                      metavar='FILE', action='store',
+                      default='/opt/gerrit2/etc/gerrit2trac.config',
+                      help='configuration (default: %default)')
+    group = OptionGroup(parser, "Parameters")
+    group.add_option('--change',
+                     metavar='<change id>', action='store', default=None,
+                     help='required')
+    group.add_option('--is-draft', dest='isdraft',
+                     metavar='<boolean>', action='store_true', default=0)
+    group.add_option('--change-url', dest='changeurl',
+                     metavar='<change url>', action='store', default=None)
+    group.add_option('--project',
+                     metavar='<project name>', action='store', default=None)
+    group.add_option('--branch',
+                     metavar='<branch>', action='store', default=None)
+    group.add_option('--topic',
+                     metavar='<topic>', action='store', default=None)
+    group.add_option('--uploader',
+                     metavar='<uploader>', action='store', default=None)
+    group.add_option('--commit',
+                     metavar='<sha1>', action='store', default=None)
+    group.add_option('--patchset',
+                     metavar='<patchset id>', action='store', default=None)
+    group.add_option('--author',
+                     metavar='<author>', action='store', default=None)
+    group.add_option('--comment',
+                     metavar='<comment>', action='store', default=None)
+    group.add_option('--Code-Review', dest='codereview',
+                     metavar='<score>', action='store', default=None)
+    group.add_option('--Verified', dest='verified',
+                     metavar='<score>', action='store', default=None)
+    group.add_option('--reviewer',
+                     metavar='<reviewer>', action='store', default=None)
+    group.add_option('--submitter',
+                     metavar='<submitter>', action='store', default=None)
+    group.add_option('--abandoner',
+                     metavar='<abandoner>', action='store', default=None)
+    group.add_option('--restorer',
+                     metavar='<restorer>', action='store', default=None)
+    group.add_option('--oldrev',
+                     metavar='<sha1>', action='store', default=None)
+    group.add_option('--newrev',
+                     metavar='<sha1>', action='store', default=None)
+    group.add_option('--refname',
+                     metavar='<ref name>', action='store', default=None)
+    group.add_option('--reason',
+                     metavar='<reason>', action='store', default=None)
+    parser.add_option_group(group)
+    (options, args) = parser.parse_args()
+    if len(args) > 2:
+        parser.error('Too many arguments')
+    if options.change is None:
+        parser.error('Missing required parameter: change')
+    return options, args
+
+
+def trac_ticket_actions(options, config, hook=None):
+    """ Define ticket update attributes."""
+
+    comment = cc = None
+    action = 'leave'
+
+    # ref-updated
+    if hook == 'ref-updated':
+        workflow = 'ref_updated'
+        comment = 'Ref updated.'
+    # patchset-created
+    elif hook == 'patchset-created':
+        workflow = 'patchset_created'
+        comment = 'Patchset created.'
+    # draft-published
+    elif hook == 'draft-published':
+        workflow = 'draft_published'
+        comment = 'Draft published'
+    # comment-added
+    elif hook == 'comment-added':
+        workflow = 'comment_added'
+        comment = 'Comment added'
+    # change-merged
+    elif hook == 'change-merged':
+        workflow = 'change_merged'
+        comment = 'Change merged.'
+        action = 'resolve'
+    # merge-failed
+    elif hook == 'merge-failed':
+        workflow = 'merge_failed'
+        comment = 'Merge failed.'
+    # change-abandoned
+    elif hook == 'change-abandoned':
+        workflow = 'change_abandoned'
+        comment = 'Change abandoned.'
+    # change-restored
+    elif hook == 'change-restored':
+        workflow = 'change_restored'
+        comment = 'Change restored.'
+    # ref-updated
+    elif hook == 'ref-updated':
+        workflow = 'ref_updated'
+        comment = 'Ref updated.'
+    # reviewer-added
+    elif hook == 'reviewer-added':
+        workflow = 'reviewer_added'
+        comment = 'Reviewer added.'
+    # topic-changed
+    elif hook == 'topic-changed':
+        workflow = 'topic_changed'
+        comment = 'Topic changed.'
+    # default
+    else:
+        workflow = None
+        comment = 'Gerrit2Trac submitted this comment.'
+
+    if config.has_option('workflow', workflow + '.comment'):
+        comment = Template(
+            config.get('workflow', workflow + '.comment')
+            ).safe_substitute(vars(options))
+    else:
+        comment = comment
+    if config.has_option('workflow', workflow + '.action'):
+        action = config.get('workflow', workflow + '.action')
+    else:
+        action = action
+    if config.has_option('workflow', workflow + '.cc'):
+        cc = Template(
+            config.get('workflow', workflow + '.cc')
+            ).safe_substitute(vars(options))
+    return comment, action, cc
+
+
+def trac_ticket_cc(ticket, cc):
+    """ Returns a formatted string of current and new ticket CC addresses. """
+
+    emails = []
+    if ticket:
+        for i, attr in enumerate(ticket):
+            if isinstance(attr, dict):
+                if attr['cc']:
+                   emails.extend([x.strip() for x in attr['cc'].split(',')])
+    if cc:
+        for addr in cc.split(','):
+            # Gerrit uses parenthesis to enclose email addresses in
+            # hook parameters.
+            # The following line was added as a work-around.
+            addr = addr.replace('(', '<').replace(')', '>')
+            if '@' in parseaddr(addr)[1]:
+                emails.extend([parseaddr(addr)[1]])
+    return ', '.join(set(emails))
+
+
+def main():
+    (options, args) = process_options()
+
+    verbose = options.v
+    hook = None
+    if args:
+        hook = args[0]
+
+    config = ConfigParser.SafeConfigParser()
+    config.read(options.c)
+    trac_url = config.get('trac', 'xmlrpcweburl')
+    trac_user = config.get('trac', 'username')
+    trac_pass = config.get('trac', 'password')
+    gerrit_url = config.get('gerrit', 'canonicalweburl')
+
+    # Query Gerrit REST API and store reponse.
+    #
+    # Gerrit includes a "magic prefix" in all REST responses which
+    # needs to be stripped in order to form a valid JSON response.
+    gerrit_request = (gerrit_url + '/changes/?q=' + options.change +
+                      '&o=CURRENT_REVISION&o=CURRENT_COMMIT&o=CURRENT_FILES')
+    try:
+        if verbose:
+            print 'Sending request: ' + gerrit_request
+        response = urlopen(gerrit_request)
+        data = []
+        if verbose:
+            print 'Reading response:'
+        for line in response:
+            if line.strip() == str(')]}\''):
+                continue
+            data.append(line)
+        response.close()
+        if data:
+            change = json.loads(''.join(data))
+    except HTTPError as e:
+        print e.code
+    except URLError as e:
+        print e.reason.args[1]
+
+    # Store commit message from JSON response string.
+    if change:
+        if verbose:
+            print 'Reading change commit message:'
+        if options.commit is None:
+            options.commit = change[0]['current_revision']
+        message = change[0]['revisions'][options.commit]['commit']['message']
+    else:
+        print 'Exiting: No results found for change ' + options.change
+        sys.exit(0)
+
+    # Check whether the commit message has a ticket reference.
+    if verbose:
+        print 'Searching commit message for ticket reference:'
+    m = re.search('^ticket:\s*#*(\d+)$', message, flags=re.I|re.M)
+    if m:
+        ticket_id = m.group(1)
+    else:
+        print 'Exiting: Ticket reference not found in commit message'
+        sys.exit(0)
+
+    # Define Trac ticket update attributes per hook.
+    (comment, action, cc) = trac_ticket_actions(options, config, hook)
+    update_attr = {
+        'action': action,
+        'ts': str(int(time() * 1000000.0))
+        }
+
+    # Query Trac XMLRPC interface and update the referenced ticket.
+    o = urlparse(trac_url)
+    trac_url_auth = (o.scheme + '://' + trac_user + ':' + trac_pass + '@' +
+                     o.netloc + o.path)
+    server = ServerProxy(trac_url_auth)
+    if cc:
+        try:
+            if verbose:
+                print 'Reading current ticket Cc: attributes:'
+            ticket = server.ticket.get(ticket_id)
+        except ProtocolError as e:
+            print 'Error: ' + e.errmsg
+            sys.exit(1)
+        if ticket:
+            update_attr['cc'] = trac_ticket_cc(ticket, cc)
+    if verbose:
+        print ('Modifications for Trac ticket ' + ticket_id + ':\n' +
+                str(update_attr))
+    try:
+        if verbose:
+            print 'Sending ticket.update() request:'
+        server.ticket.update(ticket_id, comment, update_attr, False)
+    except ProtocolError as e:
+        print 'Error: ' + e.errmsg
+        sys.exit(1)
+    if verbose:
+        print 'Successfully updated Trac ticket!'
+
+if __name__ == "__main__":
+    main()
+
